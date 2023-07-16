35c51ae866d3540733e310efd41ad04e865948d2
Riremito <29014872+Riremito@users.noreply.github.com>
2023-07-16 23:24:14 +0900

    support twms

--- EmuMain/EmuMain.cpp
+++ EmuMain/EmuMain.cpp
@@ -41,7 +41,11 @@ bool RemoveHS_EasyVer(Rosemary &r) {
 
 	//TMS157.2 HS things check
 	ULONG_PTR uHS_things = r.Scan(L"83 ?? ?? ?? ?? ?? 00 74 45 83 ?? ?? 00 A1 ?? ?? ?? ?? FF ?? ?? ?? ?? ?? 8D ?? EC 68 ?? ?? ?? ?? 50 C6 ?? ?? 02 E8");
-	r.JMP(uHS_things, uHS_things + 0x4E); //r.JMP(0x60ED4F, 0x60ED9D);
+	SCANRES(uHS_things);
+
+	if (uHS_things) {
+		r.JMP(uHS_things, uHS_things + 0x4E); //r.JMP(0x60ED4F, 0x60ED9D);
+	}
 
 	return true;
 }
@@ -51,16 +55,10 @@ bool RemoveHS_EasyVer(Rosemary &r) {
 void FixClient(Rosemary &r) {
 	// JMS v186 or before
 	AOBPatch_ADD(WindowMode_PreBB, 0x03, L"00 00 00 00");
-	// JMS v187 only, later version of bigbang client has window mode option (it will cause TMS crash)
-	//AOBPatch(WindowMode_PostBB, L"31 C0 C3");
-
 	AOBPatch(Launcher, L"B8 01 00 00 00 C3");
 	AOBPatch(Ad, L"B8 01 00 00 00 C3");
 	// v194.0 has System Settings causes crash without using this code
 	AOBPatch(MapleNetwork, L"31 C0 C2 08 00");
-
-	// TMS old default DNS (not available cause crash)
-	r.StringPatch("tw.login.maplestory.gamania.com", "127.0.0.1");
 }
 
 // Hardware BreakPoint Detection
@@ -73,19 +71,86 @@ void EmuMain() {
 	Rosemary r;
 
 	if (!RemoveCRC(r)) {
-		CRCBypass(r);
+		// if flag is true, Memory Dump Method is ignored
+		if (!GetMSDisableMemoryDump()) {
+			CRCBypass(r);
+		}
 	}
 
-	//r.Patch(0x00454AEA, L"31 C0 C3"); // hide DLL MSEA v97
-	//r.Patch(0x004C1930, L"31 C0 C3"); // hide DLL MSEA v102
-	//r.Patch(0x00AB83A0, L"83 C4 14"); // MSEA v97 MSCRC function
-	//r.JMP(0x00AB83A0 + 3, 0x00AB9C8F); // MSEA v97 MSCRC function
+	switch (GetMSRegion()) {
+	case MS_JMS: {
+		RemoveHackShield(r);
+		Disable_AntiDebug(r);
+		FixClient(r);
 
-	r.JMP(0xAC0A82, r.Scan(L"68 FF 00 00 00 6A 00 6A 00 8B ?? ?? ?? ?? ?? 83 ?? ?? ?? 6A 03 FF 15"));//TMS157.2 MSCRC ManipulatePacket
+		switch (GetMSVersion()) {
+		case 187:
+		{
+			// JMS v187 only, later version of bigbang client has window mode option (it will cause TMS crash)
+			AOBPatch(WindowMode_PostBB, L"31 C0 C3");
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
 
-	RemoveHS_EasyVer(r);
-	//RemoveHackShield(r);
+		break;
+	}
+	case MS_TWMS: {
+		RemoveHS_EasyVer(r);
+		Disable_AntiDebug(r);
+		FixClient(r);
+		// TMS old default DNS (not available cause crash)
+		ULONG_PTR uGamania = r.StringPatch("tw.login.maplestory.gamania.com", "127.0.0.1");
 
-	Disable_AntiDebug(r);
-	FixClient(r);
+		SCANRES(uGamania);
+
+		switch (GetMSVersion()) {
+		case 157:
+		{
+			r.JMP(0xAC0A82, r.Scan(L"68 FF 00 00 00 6A 00 6A 00 8B ?? ?? ?? ?? ?? 83 ?? ?? ?? 6A 03 FF 15"));//TMS157.2 MSCRC ManipulatePacket
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
+
+		break;
+	}
+	case MS_MSEA: {
+		RemoveHackShield(r);
+
+		switch (GetMSVersion()) {
+		case 97:
+		{
+			r.Patch(0x00454AEA, L"31 C0 C3"); // hide DLL MSEA v97
+			//r.Patch(0x00AB83A0, L"83 C4 14"); // MSEA v97 MSCRC function
+			//r.JMP(0x00AB83A0 + 3, 0x00AB9C8F); // MSEA v97 MSCRC function
+			break;
+		}
+		case 102:
+		{
+			r.Patch(0x004C1930, L"31 C0 C3"); // hide DLL MSEA v102
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
+
+		break;
+	}
+	default:
+	{
+		RemoveHackShield(r);
+		Disable_AntiDebug(r);
+		FixClient(r);
+		break;
+	}
+	}
 }
--- Readme.md
+++ Readme.md
@@ -1,4 +1,18 @@
 # EmuClient
+## Tested
++ JMS v164, v165, v186, v188, v194
++ TWMS v157
++ MSEA v97, v102
+
+## Settings (.ini)
++ LocalHost.ini
+	+ 設定不要
+	+ JMS以外の場合はAuthHook=1
++ EmuMain.ini
+	+ 設定不要
+	+ 特定のバージョンを指定する場合はRegionとVersionを設定する
+		+ 例: Region=MSEA, Version=102
+
 ## RunEmu
 + 単純なInjectorでEmuLoader.dllをInjectする用途で利用します
 
@@ -6,11 +20,11 @@
 + 別のDLLを読み込ませる目的で利用します
 + 実行前に読み込ませるDLLの例
 	+ FixThemida.dll
-		+ 実行前に読み込ませないとそもそもクライアントが起動しません
+		+ v164など古いバージョンでは実行前に読み込ませないとそもそもクライアントが起動しません
+		+ https://github.com/Riremito/FixThemida
 	+ LocalHost.dll
 		+ 接続先のIPをアドレスを変更するので最初に読み込ませたほうが良いです
-	+ MultiClient.dll
-		+ 多重起動を許可する場合に必要です
+		+ https://github.com/Riremito/LocalHost
 + メモリ展開後に読み込ませるDLLの例
 	+ EmuMain.dll
 		+ MSCRC Bypassを行うため、一番最初に読み込ませるべきです
@@ -18,3 +32,4 @@
 		+ その他メモリの書き換えを行うのでMSCRC Bypassより後に読み込ませる必要があります
 	+ Packet.dll	
 		+ 解析のためにPacketをフックするDLLです、無くてもよいです
+		+ https://github.com/Riremito/RirePE
